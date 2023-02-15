1dda5e1911ce8606b6001b5f6291bcd0a1a7255c
atevsl <3380033@ukr.net>
2023-02-15 10:27:17 +0200

    arrToObj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,16 +8,17 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const feedbackItems = { good, neutral, bad };
   const onLeaveFeedback = el => {
     switch (el) {
       case 'good':
-        setGood(good + 1);
+        setGood(good => good + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(neutral => neutral + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(bad => bad + 1);
         break;
       default:
         console.log('there is no such option');
@@ -47,7 +48,7 @@ const App = () => {
     >
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
+          options={feedbackItems}
           onLeaveFeedback={onLeaveFeedback}
         ></FeedbackOptions>
       </Section>
